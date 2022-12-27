088b08098b95e849c8e51174e6dc1a3f1c072833
Luke Petrolekas <reclarion@gmail.com>
2022-12-27 10:45:19 -0500

    Demonstrate jinja templating

--- hsf/main.py
+++ hsf/main.py
@@ -1,11 +1,12 @@
 from sanic import Sanic
-from sanic.response import text
+from sanic.response import Request
 
 app = Sanic("my-hello-world-app")
 
-@app.route('/')
-async def test(request):
-    return text("Done.")
+@app.get("/")
+@app.ext.template("home.html")
+async def handler(request: Request):
+    return {"seq": ["one", "two"]}
 
 if __name__ == '__main__':
     app.run()
