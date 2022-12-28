30011a86e642846440a41a5281f4f293e1e01bfc
Luke Petrolekas <reclarion@gmail.com>
2022-12-28 16:31:57 -0500

    Add initial models

--- hsf/models.py
+++ hsf/models.py
@@ -1,10 +1,32 @@
 from tortoise import Model, fields
 
-
-class Users(Model):
+class ServiceType(Model):
     id = fields.IntField(pk=True)
-    name = fields.CharField(50)
+    name = fields.CharField(128, null=False)
+    simple = fields.CharField(128)
 
-    def __str__(self):
-        return f"I am {self.name}"
- 
+class Operator(Model):
+    id = fields.IntField(pk=True)
+    name = fields.CharField(256, null=False)
+    phone = fields.CharField(20)
+    address = fields.CharField(256)
+    # total statement of all services
+    gender = fields.IntField()
+    sexpref = fields.IntField()
+    minority = fields.BooleanField()
+
+class Service(Model):
+    id = fields.IntField(pk=True)
+    operator = fields.ForeignKeyField('models.Operator')
+    service = fields.ForeignKeyField('models.ServiceType')
+     # For this specific service offered
+    gender = fields.IntField()
+    sexpref = fields.IntField()
+    minAge = fields.IntField()
+    maxAge = fields.IntField()
+    phone = fields.CharField(20)
+    rrule = fields.CharField(1000)
+    phone = fields.CharField(20)
+    address = fields.CharField(256)
+    citation = fields.CharField(500)
+    lastUpdated = fields.DatetimeField(auto_now_add=True)
